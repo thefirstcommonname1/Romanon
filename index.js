40ac3764c513f09dffaea5afc1fda3bbc5241a2d
thefirstcommonname <bakayarotinker@gmail.com>
2017-03-21 21:05:56 -0400

    Added routes folder

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 //App that sends anonymous letters using the site as the address
-var accountSid = 'ACfee6b284e3aa96a32d46a146e7fd0653';
-var authToken = 'b53068f632c663091f55f1670b82791a';
-
-//require the Twilio module and create a REST client
-var client = require('twilio')(accountSid, authToken);
-
+var route = require('./routes/routes');
 
 const express = require('express');
 const app = express();
@@ -16,21 +11,7 @@ app.get('/', function(req, res) {
   res.send("Hey there!");
 });
 
-app.post('/', function(req, res) {
-  var number = req.body.number;
-  var messages = req.body.messages;
-  client.messages.create({
-      to: "+1" + number,
-      from: "+17034578742",
-      body: messages,
-  }, function(err, message) {
-    if (err) {
-      return console.log(err);
-    }
-      console.log(message.sid);
-  });
-
-});
+app.post('/', route.sendLove);
 
 
 app.listen(3000, function() {
---
+++ routes/routes.js
@@ -0,0 +1,20 @@
+var accountSid = 'ACfee6b284e3aa96a32d46a146e7fd0653';
+var authToken = 'b53068f632c663091f55f1670b82791a';
+
+//require the Twilio module and create a REST client
+var client = require('twilio')(accountSid, authToken);
+
+module.exports.sendLove = function(req, res) {
+  var number = req.body.number;
+  var messages = req.body.messages;
+  client.messages.create({
+      to: "+1" + number,
+      from: "+17034578742",
+      body: messages,
+  }, function(err, message) {
+    if (err) {
+      return console.log(err);
+    }
+      console.log(message.sid);
+  });
+};
