9d51e02214e249cbece3482051ad2552d8c1d5a6
thefirstcommonname <bakayarotinker@gmail.com>
2017-03-31 20:22:32 -0400

    fixed heroku error

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ app.get('/', function(req, res) {
 app.post('/', route.sendLove);
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Listening on port 3000");
 });
